741f069edcd0ba725c529cebe92c074f8f1c13fc
kathy <katheqian@gmail.com>
2020-09-14 03:23:04 -0500

    test

--- static/script.js
+++ static/script.js
@@ -11,7 +11,7 @@
 $('body').on('click', '[data-toggle="tab"]', function(e) {
   e.preventDefault();
   // Select our target
-  var $target = $($(this).data('target'));
+  var target = $(this).attr('target');
   console.log(target);
 
   // Hide all tabs
