9cbac080badf3323a6d290a6863b354b5b87c12f
kathy <katheqian@gmail.com>
2020-09-14 02:55:49 -0500

    test

--- static/script.js
+++ static/script.js
@@ -7,21 +7,6 @@
   });
 }); */
 
-// Native JS version
-// Working Fiddle : https://jsfiddle.net/d34cbtw7/
-var togglers = document.querySelectorAll('[data-toggle="tab"]');
-
-for (var i = 0; i < togglers.length; i++) {
-    togglers[i].addEventListener('click', function() {
-        var tabs = document.querySelectorAll('.tab');
-        for(var j = 0; j < tabs.length; j++) {
-            tabs[j].classList.remove('active');
-        }
-        var $target = document.querySelector(this.getAttribute('data-target'));
-        $target.classList.add('active');
-    });
-}
-
 // jQuery version
 $('body').on('click', '[data-toggle="tab"]', function(e) {
   e.preventDefault();
